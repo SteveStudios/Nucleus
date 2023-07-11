05a3b594690f3d2a75e144098d35cb8fd13bd1b2
Stephen Byrne <stevestudiosgames@gmail.com>
2023-07-11 12:00:51 -0400

    .

--- x.py
+++ x.py
@@ -96,6 +96,6 @@ if (useQemu.replace(" ", "") != "y" and useQemu.replace(" ", "") != "n"):
 
 match useQemu.replace(" ", ""):
     case "y":
-        subprocess.run(["qemu-system-x86_64" "-boot" "d" "-cdrom" '"bin/iso/AtomOS.iso"' "-m" "2048"])
+        subprocess.run(["qemu-system-x86_64", "-boot", "d", "-cdrom", 'bin/iso/AtomOS.iso', "-m", "2048"])
     case "n":
         exit(0)
